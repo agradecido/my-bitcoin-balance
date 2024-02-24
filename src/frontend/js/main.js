e76e5c4477801a11f5c02ebb8c15eedda0197869
Javier <agradecido@manerasdevivir.com>
2024-02-24 12:41:11 +0100

    remove database from repository

--- src/frontend/js/main.js
+++ src/frontend/js/main.js
@@ -2,3 +2,13 @@ document.addEventListener('DOMContentLoaded', (event) => {
     const fechaHoy = new Date().toISOString().split('T')[0];
     document.getElementById('date').value = fechaHoy;
 });
+
+document.addEventListener('htmx:afterOnLoad', function(event) {
+    console.log('htmx:afterOnLoad: ' + event.detail.elt.id)
+    if (event.detail.elt.id === 'bitcoin-price') {
+        // Asumiendo que la respuesta es un JSON con la estructura { "EUR": precio }
+        const data = JSON.parse(event.detail.elt.innerText);
+        const price = data.EUR.price; // Ajusta esta línea según la estructura exacta de tu JSON
+        event.detail.elt.innerText = `€${price}`;
+    }
+});
